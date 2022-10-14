5836ef0e3d51cb04192ff35172376e126101d6ca
Frederic Bui <bui.frederic.pro@gmail.com>
2022-10-14 23:04:47 +0200

    display navbar & banner

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import "./App.css";
 import Row from "./Row";
-import request from "./request";
+import requests from "./requests";
+import Banner from "./Banner";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Banner />
       <Row
         title="NETFLIX ORIGINALS"
-        fetchURL={request.fetchNetflixOriginals}
+        fetchURL={requests.fetchNetflixOriginals}
         isLargeRow
       />
-      <Row title="Trending Now" fetchURL={request.fetchTrending} />
-      <Row title="Action Movies" fetchURL={request.fetchActionMovies} />
-      <Row title="Comedy Movies" fetchURL={request.fetchComedyMovies} />
-      <Row title="Romance Movies" fetchURL={request.fetchRomanceMovies} />
+      <Row title="Trending Now" fetchURL={requests.fetchTrending} />
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+      <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies} />
     </div>
   );
 }
